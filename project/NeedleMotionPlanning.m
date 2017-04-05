8be02cb743f26fa933e3c83301d8e3f4624eeb5b
ankur0993 <ankur.agrawal2409@gmail.com>
2017-04-05 13:44:49 -0400

    Update

--- project/NeedleMotionPlanning.m
+++ project/NeedleMotionPlanning.m
@@ -2,11 +2,47 @@ clear
 clc
 close all
 
-theta=[120:0.01:240];
+theta=[90:0.01:210];
 Needle.Needle=[5*cosd(theta);5*sind(theta)];
 Needle.Center=repmat([0;0],[1,size(Needle.Needle,2)]);
-Pivot=repmat(Needle.Needle(:,6000),[1,size(Needle.Needle,2)]);      %Point of contact with tissue
+Pivot=repmat(Needle.Needle(:,12000),[1,size(Needle.Needle,2)]);      %Point of contact with tissue
 
-for i=1:40
-Needle=NeedleMovement(Needle,Pivot,0.8,4);
+x1=[-5:0.1:-3];
+y1=2*x1+6;
+x3=[3:0.1:5];
+y3=-2*x3+6;
+x2=[-3:0.1:3];
+y2=0*x2;
+x=[x1,x2,x3];
+y=[y1,y2,y3];
+
+Points=[x;y];
+axis off
+for i=1:size(x,2)
+leq=[-7:0.1:y(i)];
+Points=[Points [repmat(x(i),[1,size(leq,2)]);leq]];
 end
+
+Points=unique(Points','rows')';
+Points_grey=[];
+
+
+% subplot(2,2,1)
+% set(gca, 'XTick', [], 'YTick', [])
+% for i=1:1
+% [Points,Points_grey,Needle]=NeedleMovement(Needle,Pivot,Points,Points_grey,0,0);
+% end
+% 
+% % subplot(2,2,2)
+% figure;
+% [Points,Points_grey,Needle]=NeedleMovement(Needle,Pivot,Points,Points_grey,50,0);
+% % 
+% subplot(2,2,3)
+% figure;
+for i=1:50
+[Points,Points_grey,Needle]=NeedleMovement(Needle,Pivot,Points,Points_grey,0,1);
+end
+size(Points_grey,2)
+figure;
+[Points,Points_grey,Needle]=NeedleMovement(Needle,Pivot,Points,Points_grey,10,10);
+size(Points_grey,2)
--- project/NeedleMovement.m
+++ project/NeedleMovement.m
@@ -1,4 +1,4 @@
-function Needle=NeedleMovement(Needle,Pivot,insert, twist)
+function [Points,Points_grey,Needle]=NeedleMovement(Needle,Pivot,Points,Points_grey,insert, twist)
 
 Needle.Needle=[cosd(twist) -sind(twist);sind(twist) cosd(twist)]*(Needle.Needle-Pivot)+Pivot;
 Needle.Center=[cosd(twist) -sind(twist);sind(twist) cosd(twist)]*(Needle.Center-Pivot)+Pivot;
@@ -9,7 +9,20 @@ plot(Needle.Needle(1,:),Needle.Needle(2,:));
 hold on;
 scatter(Pivot(1), Pivot(2));
 scatter(End(1),End(2))
+
+
+Needle_round=round(Needle.Needle,1);
+Points_round=round(Points,1);
+
+[C,Ia,Ib]=intersect(Points_round',Needle_round','rows');
+Points_grey=[Points_grey Points(:,Ia)];
+Points(:,Ia)=[];
+
+scatter(Points_grey(1,:),Points_grey(2,:),'k.')
+scatter(Points(1,:),Points(2,:),'r.')
+
 axis([-10,10,-10,10]);
 axis square;
+set(gca, 'XTick', [], 'YTick', [])
 drawnow;
 end
